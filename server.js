026872f1d7210baa58075ed0318a34146bce201a
Nick Pavlovic <nickpavlovic@icloud.com>
2022-10-14 15:52:30 -0400

    POST route working

--- server.js
+++ server.js
@@ -28,7 +28,22 @@ app.get('/api/notes', (req, res) =>
 
 // POST Route for /api/notes page
 app.post('/api/notes', (req, res) => {
-  //to do - should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+  
+  const { title, text } = req.body;
+
+  if (req.body) {
+
+    const newNote = {
+      title,
+      text,
+      note_id: uuid()
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
 
 // Bonus
